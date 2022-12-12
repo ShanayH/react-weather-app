453667d44443bbf133fcdf85e0d51d6c65c519b1
ShanayH <shanay.taylor@hotmail.com>
2022-12-12 13:41:11 -0600

    added rel="noreferrer" to fix netlify error

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <h1> Weather App Test</h1>
       <footer>
         This project was coded by Shanay Hazell and is {""}
-        <a href="https://github.com/ShanayH/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/ShanayH/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on GitHub
         </a>
         {/* later you can add a src to your personal page as well */}
