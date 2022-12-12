65158c3cbaa850b6a523f99049b4d9e9572c9579
ShanayH <shanay.taylor@hotmail.com>
2022-12-12 14:41:25 -0600

    Add css

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      {" "}
-      <h1> Weather App Test</h1>
-      <footer>
-        This project was coded by Shanay Hazell and is {""}
-        <a
-          href="https://github.com/ShanayH/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-        {/* later you can add a src to your personal page as well */}
-      </footer>
+      <div className="container">
+        <div className="body">
+          <h1> Weather App </h1>
+          
+        </div>
+        <footer>
+          This project was coded by Shanay Hazell and is {""}
+          <a
+            href="https://github.com/ShanayH/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+          {/* later you can add a src to your personal page as well */}
+        </footer>
+      </div>
     </div>
   );
 }
