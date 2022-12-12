6b634d84121d0f817178ae718fec55ce418c8f91
ShanayH <shanay.taylor@hotmail.com>
2022-12-12 13:35:07 -0600

    Added the github url

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-
 import "./App.css";
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by Shanay Hazell and is {""}
+        <a href="https://github.com/ShanayH/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+        {/* later you can add a src to your personal page as well */}
+      </footer>
+    </div>
+  );
 }
 
 export default App;
