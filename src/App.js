fb0bcd98b0fefc4fd7e4f1815c5e2532cf58ecb7
ShanayH <shanay.taylor@hotmail.com>
2022-12-12 16:38:01 -0600

    html and css almost complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <body>
       <div className="App">
         <div className="container">
-          <h1> Weather App </h1>
+          <Weather />
         </div>
 
         <footer>
---
+++ src/Weather.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          autoComplete="off"
+          autoFocus="on"
+          className="search-bar"
+        />
+        <input type="submit" value="Search" classNamem="search-button" />
+      </form>
+      <div className="row">
+        <div className="col-6">
+          <h1 className="city-result"> Dublin, Ireland </h1>
+        </div>
+        <div className="col-6 date-time">
+          <ul>
+            <li>Wednesday 07:00</li>
+            <li>Mostly Cloudy</li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-6">
+          <ul>
+            <li>
+              <img
+                src="http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/sun-behind-small-cloud.png"
+                alt="mostly cloudy"
+                width="50"
+              />
+              <span className="temp-result">6 </span>
+
+              <span className="units">
+                <a href="#" className="active">
+                  °C {""}
+                </a>
+                <a href="#">°F</a>
+              </span>
+            </li>
+          </ul>
+        </div>
+        <div class="col-6 weather-info">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity 30%</li>
+            <li>Wind: 13 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
