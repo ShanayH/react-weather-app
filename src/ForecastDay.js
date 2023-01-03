58883af1a22bfbb1b6a287dde8f37aead710e4c8
ShanayH <shanay.taylor@hotmail.com>
2023-01-03 14:07:51 -0600

    Update ForecastDay.js
    Added minTemp and maxTemp functions, and made day function

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -1,9 +1,28 @@
 import React from "react";
 
 export default function ForecastDay(props) {
+  function minTemp() {
+    let temperature = Math.round(props.info.data.list[0].main.temp_min);
+    return `${temperature}°`;
+  }
+
+  function maxTemp() {
+    let temperature = Math.round(props.info.data.list[0].main.temp_max);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.info.data.list[0].dt * 1000);
+    let day = date.getDay(); 
+
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",]
+
+    return days[day];
+  }
+
   return (
     <div className="forecastDay">
-      <div className="forecast-day">{props.info.data.list[0].dt}</div>
+      <div className="forecast-day">{day()}</div>
       <img
         src="http://openweathermap.org/img/wn/10d@2x.png"
         // src="http://openweathermap.org/img/wn/(${props.info.data.list[0].weather[0].icon})@2x.png"
@@ -11,12 +30,8 @@ export default function ForecastDay(props) {
         width={44}
       />
       <div className="forecast-temps">
-        <span className="forecast-temp-min">
-          {props.info.data.list[2].main.temp_min}°
-        </span>
-        <span className="forecast-temp-max">
-          {props.info.data.list[2].main.temp_max}°
-        </span>
+        <span className="forecast-temp-min">{minTemp()}</span>
+        <span className="forecast-temp-max">{maxTemp()}</span>
       </div>
     </div>
   );
