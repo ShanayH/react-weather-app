b69fc2a23bb3de0f9177472c3fb62efb46a5835f
ShanayH <shanay.taylor@hotmail.com>
2023-01-15 12:27:55 -0600

    Update ForecastDay.js

--- src/ForecastDay.js
+++ src/ForecastDay.js
@@ -24,6 +24,7 @@ export default function ForecastDay(props) {
   return (
     <div className="forecastDay">
       <div className="forecast-day">{day()}</div>
+    
       <WeatherIcon icon={props.info.weather[0].icon} />
 
       <div className="forecast-temps">
